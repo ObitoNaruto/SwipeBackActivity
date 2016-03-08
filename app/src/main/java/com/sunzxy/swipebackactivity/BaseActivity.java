5da579a76c85dc367c37b29a7a32f7789b7ffe34
郑晓勇 <zhengxiaoyong@weidian.com>
2016-03-08 17:13:50 +0800

    no message

--- app/src/main/java/com/sunzxy/swipebackactivity/BaseActivity.java
+++ app/src/main/java/com/sunzxy/swipebackactivity/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.sunzxy.swipebackactivity;
 
-import com.sunzxy.library.SwipeBackActivity;
+
+import com.sunzxy.swipeback.SwipeBackActivity;
 
 /**
  * Created by zhengxiaoyong on 16/3/5.
